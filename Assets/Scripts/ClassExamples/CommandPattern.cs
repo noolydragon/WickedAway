9bfc9719569642ea0dc67924faa29ae44bc6a8da
Cainan Patterson <pattersoncainan@gmail.com>
2026-09-09 12:34:59 -0500

    Added simple terrain and Class example

--- Assets/Scripts/ClassExamples/CommandPattern.cs
+++ Assets/Scripts/ClassExamples/CommandPattern.cs
@@ -1,22 +1,54 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 
 [DisallowMultipleComponent]
 
 public class CommandPattern : MonoBehaviour
 {
     public float moveDistance = 1.0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private readonly Stack<ICommand> commandHistory = new Stack<ICommand>();
+    public int UndoCount = 0;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W))
+            ExecuteMoveCommand(Vector3.forward * moveDistance);
+        if(Input.GetKeyDown(KeyCode.A))
+            ExecuteMoveCommand(Vector3.right * -moveDistance);
+        if(Input.GetKeyDown(KeyCode.S))
+            ExecuteMoveCommand(Vector3.forward * -moveDistance);
+        if(Input.GetKeyDown(KeyCode.D))
+            ExecuteMoveCommand(Vector3.right * moveDistance);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Undo();
+        }
+    }
+
+    public void Undo()
+    {
+        if (commandHistory.Count == 0)
+            return;
+        ICommand command = commandHistory.Pop();
+        command.Undo();
+        UndoCount--;
+    }
+    void ExecuteMoveCommand(Vector3 _amount)
+    {
+        ICommand command = new MoveCommand(transform, _amount);
+        command.Execute();
+        commandHistory.Push(command);
+        UndoCount++;
     }
 }
 
@@ -28,14 +60,32 @@ public interface ICommand
 
 public class MoveCommand : ICommand
 {
-    private readonly Action executeAction; //basically is a void pointer, like function pointer, can set executeAction to be any function
-    public void Execute()
+    private readonly Action execute;
+    private readonly Action undo;
+
+    private Vector3 startingPosition;
+
+    public MoveCommand(Transform _transform, Vector3 _moveAmount)
     {
-        
+        execute = () =>
+        {
+            startingPosition = _transform.position;
+            _transform.position += _moveAmount;
+        };
+
+        undo = () => _transform.position = startingPosition;
     }
 
+    public void Execute()
+    {
+        execute();
+    }
     public void Undo()
     {
-        
+        undo();
     }
 }
+
+#if UNITY_EDITOR
+
+#endif
